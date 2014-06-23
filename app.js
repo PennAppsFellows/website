acc7069bee2452d36b3a6661fd8b13285745a4c6
Alican Mutus <almutus@gmail.com>
2014-06-22 21:15:44 -0400

    first attempt at using mongoose to insert votes

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 var express = require('express');
-var app = express();        
+var app = express();
+
+//Mongoose and connection to MongoDB
+var mongoose = require('mongoose');
+ongoose.connect('mongodb://localhost/test');
+
 
 app.set('port', (process.env.PORT || 8080))
 app.use(express.static(__dirname + '/views'));
@@ -11,6 +16,25 @@ app.get('/', function(req, res) {
 
 app.get('/projectphl', function(req, res) {
 	res.render('projectphl.ejs');
+});
+
+app.post('/vote', function(req, res) {
+	var email = req.params.email;
+	var idea = req.params.idea;
+
+	var VoteSchema = new Schema({
+		email: {type: "String"},
+	 	idea: {type: "String"}
+	});
+
+	var Vote = mongoose.model('Vote', VoteSchema);
+
+	var query = Vote.findOne({ 'email': email });
+
+	if(!query) {
+		var vote = new Vote({email: email, idea: idea});
+	}
+
 });
 
 app.listen(app.get('port'));
