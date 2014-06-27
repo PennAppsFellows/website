aa269488ce85b85a82a30ea6aa950318cae243c1
Cole Kettler <cole.kettler@gmail.com>
2014-06-27 02:25:09 -0400

    Validate required fields for voting

--- views/media/js/submit-vote.js
+++ views/media/js/submit-vote.js
@@ -1,8 +1,6 @@
 $(function() {
     // Submit vote data.
-    var submitVote = function(e) {
-        e.preventDefault();
-
+    var submitVote = function() {
         var $this = $(this);
 
         return $.ajax({
@@ -11,9 +9,42 @@ $(function() {
             dataType: 'json',
             data: $this.serialize()
         });
+
+        $.post('', $(this).serialize(), function() {}, function() {});
     };
 
-    // Events.
+    // Validation
+    $('#vote-form').bootstrapValidator({
+        live: 'disabled',
+        message: 'This field is required',
+        submitButtons: 'input[type="submit"]',
+        submitHandler: function(validator, form, btn) {
+            var xhr = $.post('/vote', form.serialize());
+        },
+        fields: {
+            email: {
+                enabled: true,
+                validators: {
+                    emailAddress: {
+                        message: 'Invalid email address'
+                    },
+                    notEmpty: {
+                        message: 'Email address is required'
+                    }
+                }
+            },
+            idea: {
+                enabled: true,
+                validators: {
+                    notEmpty: {
+                        message: 'No idea selected'
+                    }
+                }
+            }
+        }
+    });
+
+    // Events
     $('form#vote').submit(function(e) {
         var req = submitVote(e);
     });
